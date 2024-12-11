de945754a122de11c8441e99e4eb708e6ddba11f
Lovish Bansal <lovishbansal441@gmail.com>
2024-12-12 04:44:11 +0530

    [ENV Fix]

--- frontend/src/context/user.context.jsx
+++ frontend/src/context/user.context.jsx
@@ -15,7 +15,7 @@ export const UserContext = createContext({
 });
 
 const API_BASE_URL =
-    (import.meta.env.API_URL || 'http://localhost:4000') +
+    (import.meta.env.VITE_API_URL || 'http://localhost:4000') +
     '/api/users';
 
 export const UserProvider = ({ children }) => {
